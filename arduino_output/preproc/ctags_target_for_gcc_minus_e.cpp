af6d375a9789d99acfa47b9dd5d94c6cbdcac4c6
Harris <yuopres@gmail.com>
2024-05-17 16:48:54 +0100

    stop tracking arduino_outup file. add simple_blue.ino

--- arduino_output/preproc/ctags_target_for_gcc_minus_e.cpp
+++ arduino_output/preproc/ctags_target_for_gcc_minus_e.cpp
@@ -1,55 +1,171 @@
-# 1 "/Users/harris/Documents/y1/final_project/ultrasonic_distance/ultrasonic_distance.ino"
-/*********
-  Rui Santos
-  Complete project details at https://RandomNerdTutorials.com/esp32-hc-sr04-ultrasonic-arduino/
-  
-  Permission is hereby granted, free of charge, to any person obtaining a copy
-  of this software and associated documentation files.
-  
-  The above copyright notice and this permission notice shall be included in all
-  copies or substantial portions of the Software.
-*********/
+# 1 "/Users/harris/Documents/y1/final_project/simple_blue/simple_blue.ino"
+# 2 "/Users/harris/Documents/y1/final_project/simple_blue/simple_blue.ino" 2
 
-const int trigPin = 5;
-const int echoPin = 18;
+ControllerPtr myControllers[
+# 3 "/Users/harris/Documents/y1/final_project/simple_blue/simple_blue.ino" 3 4
+                           4
+# 3 "/Users/harris/Documents/y1/final_project/simple_blue/simple_blue.ino"
+                                            ];
 
-//define sound speed in cm/uS
-
-
-
-long duration;
-float distanceCm;
-float distanceInch;
-
-void setup() {
-  Serial.begin(115200); // Starts the serial communication
-  pinMode(trigPin, 0x03); // Sets the trigPin as an Output
-  pinMode(echoPin, 0x01); // Sets the echoPin as an Input
+// This callback gets called any time a new gamepad is connected.
+// Up to 4 gamepads can be connected at the same time.
+void onConnectedController(ControllerPtr ctl) {
+    bool foundEmptySlot = false;
+    for (int i = 0; i < 
+# 9 "/Users/harris/Documents/y1/final_project/simple_blue/simple_blue.ino" 3 4
+                       4
+# 9 "/Users/harris/Documents/y1/final_project/simple_blue/simple_blue.ino"
+                                        ; i++) {
+        if (myControllers[i] == nullptr) {
+            Serial.printf("CALLBACK: Controller is connected, index=%d\n", i);
+            // Additionally, you can get certain gamepad properties like:
+            // Model, VID, PID, BTAddr, flags, etc.
+            ControllerProperties properties = ctl->getProperties();
+            Serial.printf("Controller model: %s, VID=0x%04x, PID=0x%04x\n", ctl->getModelName().c_str(), properties.vendor_id,
+                           properties.product_id);
+            myControllers[i] = ctl;
+            foundEmptySlot = true;
+            break;
+        }
+    }
+    if (!foundEmptySlot) {
+        Serial.println("CALLBACK: Controller connected, but could not found empty slot");
+    }
 }
 
+void onDisconnectedController(ControllerPtr ctl) {
+    bool foundController = false;
+
+    for (int i = 0; i < 
+# 30 "/Users/harris/Documents/y1/final_project/simple_blue/simple_blue.ino" 3 4
+                       4
+# 30 "/Users/harris/Documents/y1/final_project/simple_blue/simple_blue.ino"
+                                        ; i++) {
+        if (myControllers[i] == ctl) {
+            Serial.printf("CALLBACK: Controller disconnected from index=%d\n", i);
+            myControllers[i] = nullptr;
+            foundController = true;
+            break;
+        }
+    }
+
+    if (!foundController) {
+        Serial.println("CALLBACK: Controller disconnected, but not found in myControllers");
+    }
+}
+
+void dumpGamepad(ControllerPtr ctl) {
+    Serial.printf(
+        //"idx=%d, dpad: 0x%02x, buttons: 0x%04x, misc: 0x%02x\n",
+        "Lx: %4d, Ly: %4d\n",
+        //"buttons: 0x%05x\n",
+        //"Lx: %4d, Ly: %4d, Rx: %4d, Ry: %4d, brake: %4d, throttle: %4d\n",
+        //ctl->index(),        // Controller Index
+        //ctl->dpad(),         // D-pad
+        //ctl->buttons()//,      // bitmask of pressed buttons
+        //ctl->miscButtons(),   // bitmask of pressed "misc" buttons
+        ctl->axisX(), // (-511 - 512) left X Axis
+        ctl->axisY()//,        // (-511 - 512) left Y axis
+        //ctl->axisRX(),       // (-511 - 512) right X axis
+        //ctl->axisRY(),       // (-511 - 512) right Y axis
+        //ctl->brake(),        // (0 - 1023): brake button
+        //ctl->throttle()      // (0 - 1023): throttle (AKA gas) button
+    );
+}
+
+void processGamepad(ControllerPtr ctl) {
+    // Query controller data by getting the buttons() function.
+    //dumpGamepad(ctl);
+
+    // A button relates to top gripper
+    // press left trigger simultaneously to open gripper
+    // press right trigger simultaneously to close gripper
+    if (ctl->buttons() == 0x0018) {
+        Serial.println("open top gripper");
+    } else if (ctl->buttons() == 0x0028) {
+        Serial.println("close top gripper");
+    }
+
+    // B button relates to bottom gripper
+    // press left trigger simultaneously to open gripper
+    // press right trigger simultaneously to close gripper
+    else if (ctl->buttons() == 0x0011) {
+        Serial.println("open bottom gripper");
+    } else if (ctl->buttons() == 0x0021) {
+        Serial.println("close bottom gripper");
+    }
+
+    // X button relates to rack and pinion
+    // press left trigger simultaneously to move rack and pinion up
+    // press right trigger simultaneously to move rack and pinion down
+    else if (ctl->buttons() == 0x0014) {
+        Serial.println("move rack and pinion up");
+    } else if (ctl->buttons() == 0x0024) {
+        Serial.println("move rack and pinion down");
+    }
+
+    // Y button relates to wheels
+    // press left trigger simultaneously to move robot left
+    // press right trigger simultaneously to move robot right
+    // or alternatively, move left joystick left or right
+    else if (ctl->buttons() == 0x0012 || ctl->axisX() < -200) {
+        Serial.println("move robot left");
+    } else if (ctl->buttons() == 0x0022 || ctl->axisX() > 200) {
+        Serial.println("move robot right");
+    }
+}
+
+void processControllers() {
+    for (auto myController : myControllers) {
+        if (myController && myController->isConnected() && myController->hasData()) {
+            if (myController->isGamepad()) {
+                processGamepad(myController);
+            } else {
+                Serial.println("Unsupported controller");
+            }
+        }
+    }
+}
+
+// Arduino setup function. Runs in CPU 1
+void setup() {
+    Serial.begin(115200);
+    Serial.printf("Firmware: %s\n", BP32.firmwareVersion());
+    const uint8_t* addr = BP32.localBdAddress();
+    Serial.printf("BD Addr: %2X:%2X:%2X:%2X:%2X:%2X\n", addr[0], addr[1], addr[2], addr[3], addr[4], addr[5]);
+
+    // Setup the Bluepad32 callbacks
+    BP32.setup(&onConnectedController, &onDisconnectedController);
+
+    // "forgetBluetoothKeys()" should be called when the user performs
+    // a "device factory reset", or similar.
+    // Calling "forgetBluetoothKeys" in setup() just as an example.
+    // Forgetting Bluetooth keys prevents "paired" gamepads to reconnect.
+    // But it might also fix some connection / re-connection issues.
+    BP32.forgetBluetoothKeys();
+
+    // Enables mouse / touchpad support for gamepads that support them.
+    // When enabled, controllers like DualSense and DualShock4 generate two connected devices:
+    // - First one: the gamepad
+    // - Second one, which is a "virtual device", is a mouse.
+    // By default, it is disabled.
+    BP32.enableVirtualDevice(false);
+}
+
+// Arduino loop function. Runs in CPU 1.
 void loop() {
-  // Clears the trigPin
-  digitalWrite(trigPin, 0x0);
-  delayMicroseconds(2);
-  // Sets the trigPin on HIGH state for 10 micro seconds
-  digitalWrite(trigPin, 0x1);
-  delayMicroseconds(10);
-  digitalWrite(trigPin, 0x0);
+    // This call fetches all the controllers' data.
+    // Call this function in your main loop.
+    bool dataUpdated = BP32.update();
+    if (dataUpdated)
+        processControllers();
 
-  // Reads the echoPin, returns the sound wave travel time in microseconds
-  duration = pulseIn(echoPin, 0x1);
+    // The main loop must have some kind of "yield to lower priority task" event.
+    // Otherwise, the watchdog will get triggered.
+    // If your main loop doesn't have one, just add a simple `vTaskDelay(1)`.
+    // Detailed info here:
+    // https://stackoverflow.com/questions/66278271/task-watchdog-got-triggered-the-tasks-did-not-reset-the-watchdog-in-time
 
-  // Calculate the distance
-  distanceCm = duration * 0.034/2;
-
-  // Convert to inches
-  distanceInch = distanceCm * 0.393701;
-
-  // Prints the distance in the Serial Monitor
-  //Serial.print("Distance (cm): ");
-  //Serial.println(distanceCm);
-  // Serial.print("Distance (inch): ");
-  // Serial.println(distanceInch);
-
-  delay(10);
+    //     vTaskDelay(1);
+    delay(150);
 }
---
+++ simple_blue/simple_blue.ino
@@ -0,0 +1,158 @@
+#include <Bluepad32.h>
+
+ControllerPtr myControllers[BP32_MAX_GAMEPADS];
+
+// This callback gets called any time a new gamepad is connected.
+// Up to 4 gamepads can be connected at the same time.
+void onConnectedController(ControllerPtr ctl) {
+    bool foundEmptySlot = false;
+    for (int i = 0; i < BP32_MAX_GAMEPADS; i++) {
+        if (myControllers[i] == nullptr) {
+            Serial.printf("CALLBACK: Controller is connected, index=%d\n", i);
+            // Additionally, you can get certain gamepad properties like:
+            // Model, VID, PID, BTAddr, flags, etc.
+            ControllerProperties properties = ctl->getProperties();
+            Serial.printf("Controller model: %s, VID=0x%04x, PID=0x%04x\n", ctl->getModelName().c_str(), properties.vendor_id,
+                           properties.product_id);
+            myControllers[i] = ctl;
+            foundEmptySlot = true;
+            break;
+        }
+    }
+    if (!foundEmptySlot) {
+        Serial.println("CALLBACK: Controller connected, but could not found empty slot");
+    }
+}
+
+void onDisconnectedController(ControllerPtr ctl) {
+    bool foundController = false;
+
+    for (int i = 0; i < BP32_MAX_GAMEPADS; i++) {
+        if (myControllers[i] == ctl) {
+            Serial.printf("CALLBACK: Controller disconnected from index=%d\n", i);
+            myControllers[i] = nullptr;
+            foundController = true;
+            break;
+        }
+    }
+
+    if (!foundController) {
+        Serial.println("CALLBACK: Controller disconnected, but not found in myControllers");
+    }
+}
+
+void dumpGamepad(ControllerPtr ctl) {
+    Serial.printf(
+        //"idx=%d, dpad: 0x%02x, buttons: 0x%04x, misc: 0x%02x\n",
+        "Lx: %4d, Ly: %4d\n",
+        //"buttons: 0x%05x\n",
+        //"Lx: %4d, Ly: %4d, Rx: %4d, Ry: %4d, brake: %4d, throttle: %4d\n",
+        //ctl->index(),        // Controller Index
+        //ctl->dpad(),         // D-pad
+        //ctl->buttons()//,      // bitmask of pressed buttons
+        //ctl->miscButtons(),   // bitmask of pressed "misc" buttons
+        ctl->axisX(),        // (-511 - 512) left X Axis
+        ctl->axisY()//,        // (-511 - 512) left Y axis
+        //ctl->axisRX(),       // (-511 - 512) right X axis
+        //ctl->axisRY(),       // (-511 - 512) right Y axis
+        //ctl->brake(),        // (0 - 1023): brake button
+        //ctl->throttle()      // (0 - 1023): throttle (AKA gas) button
+    );
+}
+
+void processGamepad(ControllerPtr ctl) {
+    // Query controller data by getting the buttons() function.
+    //dumpGamepad(ctl);
+
+    // A button relates to top gripper
+    // press left trigger simultaneously to open gripper
+    // press right trigger simultaneously to close gripper
+    if (ctl->buttons() == 0x0018) {
+        Serial.println("open top gripper");
+    } else if (ctl->buttons() == 0x0028) {
+        Serial.println("close top gripper");
+    }
+
+    // B button relates to bottom gripper
+    // press left trigger simultaneously to open gripper
+    // press right trigger simultaneously to close gripper
+    else if (ctl->buttons() == 0x0011) {
+        Serial.println("open bottom gripper");
+    } else if (ctl->buttons() == 0x0021) {
+        Serial.println("close bottom gripper");
+    }
+
+    // X button relates to rack and pinion
+    // press left trigger simultaneously to move rack and pinion up
+    // press right trigger simultaneously to move rack and pinion down
+    else if (ctl->buttons() == 0x0014) {
+        Serial.println("move rack and pinion up");
+    } else if (ctl->buttons() == 0x0024) {
+        Serial.println("move rack and pinion down");
+    }
+
+    // Y button relates to wheels
+    // press left trigger simultaneously to move robot left
+    // press right trigger simultaneously to move robot right
+    // or alternatively, move left joystick left or right
+    else if (ctl->buttons() == 0x0012 || ctl->axisX() < -200) {
+        Serial.println("move robot left");
+    } else if (ctl->buttons() == 0x0022 || ctl->axisX() > 200) {
+        Serial.println("move robot right");
+    }
+}
+
+void processControllers() {
+    for (auto myController : myControllers) {
+        if (myController && myController->isConnected() && myController->hasData()) {
+            if (myController->isGamepad()) {
+                processGamepad(myController);
+            } else {
+                Serial.println("Unsupported controller");
+            }
+        }
+    }
+}
+
+// Arduino setup function. Runs in CPU 1
+void setup() {
+    Serial.begin(115200);
+    Serial.printf("Firmware: %s\n", BP32.firmwareVersion());
+    const uint8_t* addr = BP32.localBdAddress();
+    Serial.printf("BD Addr: %2X:%2X:%2X:%2X:%2X:%2X\n", addr[0], addr[1], addr[2], addr[3], addr[4], addr[5]);
+
+    // Setup the Bluepad32 callbacks
+    BP32.setup(&onConnectedController, &onDisconnectedController);
+
+    // "forgetBluetoothKeys()" should be called when the user performs
+    // a "device factory reset", or similar.
+    // Calling "forgetBluetoothKeys" in setup() just as an example.
+    // Forgetting Bluetooth keys prevents "paired" gamepads to reconnect.
+    // But it might also fix some connection / re-connection issues.
+    BP32.forgetBluetoothKeys();
+
+    // Enables mouse / touchpad support for gamepads that support them.
+    // When enabled, controllers like DualSense and DualShock4 generate two connected devices:
+    // - First one: the gamepad
+    // - Second one, which is a "virtual device", is a mouse.
+    // By default, it is disabled.
+    BP32.enableVirtualDevice(false);
+}
+
+// Arduino loop function. Runs in CPU 1.
+void loop() {
+    // This call fetches all the controllers' data.
+    // Call this function in your main loop.
+    bool dataUpdated = BP32.update();
+    if (dataUpdated)
+        processControllers();
+
+    // The main loop must have some kind of "yield to lower priority task" event.
+    // Otherwise, the watchdog will get triggered.
+    // If your main loop doesn't have one, just add a simple `vTaskDelay(1)`.
+    // Detailed info here:
+    // https://stackoverflow.com/questions/66278271/task-watchdog-got-triggered-the-tasks-did-not-reset-the-watchdog-in-time
+
+    //     vTaskDelay(1);
+    delay(150);
+}
